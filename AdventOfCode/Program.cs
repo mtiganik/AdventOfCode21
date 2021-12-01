0afe87957267b636860624f89f4a7fa6dbf89c23
Mihkel Tiganik <mtiganik@gmail.com>
2021-12-01 17:19:53 +0200

    Both tasks

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -14,6 +14,54 @@ namespace AdventOfCode
 
             StreamReader sr = new StreamReader("input.txt");
             string line = sr.ReadLine();
+            int increasecounter = 0;
+
+
+            int prev2x = int.Parse(line);
+            line = sr.ReadLine();
+            int prev = int.Parse(line);
+            int sumOfdigits = 0;
+            int current;
+            int prevSum = 0; ;
+            line= sr.ReadLine();
+            string writeString;
+            while (line != null)
+            {
+                current = int.Parse(line);
+                sumOfdigits = prev2x + prev + current;
+
+                if (sumOfdigits > prevSum && prevSum != 0)
+                {
+                    increasecounter++;
+                    writeString = "Increased";
+
+                }
+                else
+                {
+                    writeString = "Not increased";
+
+                }
+                Console.WriteLine(sumOfdigits + writeString);
+
+                //everything ready for new cycle
+                prev2x = prev;
+                prev = current;
+
+                prevSum = sumOfdigits;
+                line = sr.ReadLine();
+
+                
+            }
+            Console.WriteLine(increasecounter);
+            Console.ReadLine();
+
+
+        }
+
+        static void firstTask()
+        {
+            StreamReader sr = new StreamReader("input.txt");
+            string line = sr.ReadLine();
 
             int current = int.Parse(line);
             int previous;
@@ -31,12 +79,10 @@ namespace AdventOfCode
                 }
                 line = sr.ReadLine();
 
-                
+
             }
             Console.WriteLine(increasecounter);
             Console.ReadLine();
-
-
         }
 
 
